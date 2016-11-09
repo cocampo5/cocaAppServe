4b725817eec9bb411dee289432d6a688b85cf40b
Cristobal Ocampo Quintero <cocampo503@gmail.com>
2016-11-09 04:23:36 +0000

    Añadiendo migrate

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module CocaApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.web_console.whitelisted_ips = '190.249.22.189'
+    #config.web_console.whitelisted_ips = '190.249.22.189'
+    
   end
 end
