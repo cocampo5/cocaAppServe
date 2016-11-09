e4cf4c45648bde103b5ad1f54d743ed3d0282d9d
Cristobal Ocampo Quintero <cocampo5@eafit.edu.co>
2016-11-09 00:36:21 -0500

    Modificaciones menores

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :vendedors
   get 'vendedores/index'
-  root	:to =>	'vendedors#new'
-  patch "vendedors/edit" => "vendedors#edit", :as => "vendedors/edit"
+  root	:to =>	'vendedors#index'
+  #patch "vendedors/edit" => "vendedors#edit", :as => "vendedors/edit"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
