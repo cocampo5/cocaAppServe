72f84b16b60c72fe30b8d59289fb568295505c0e
Cristobal Ocampo Quintero <cocampo503@gmail.com>
2016-11-09 04:55:18 +0000

    Añadiendo migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161109042047) do
+ActiveRecord::Schema.define(version: 20161109045412) do
 
   create_table "vendedors", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "nombre"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20161109042047) do
     t.string   "email"
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
+    t.string   "pass"
   end
 
 end
